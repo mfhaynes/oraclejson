21e319b4b11ae1bf3c672cb60fd338e58c5965b4
Michael Haynes <mf.haynes@gmail.com>
2021-02-08 12:49:12 -0500

    Create create_topic_experts_table.sql

---
+++ create_topic_experts_table.sql
@@ -0,0 +1,3 @@
+create table topic_experts (topic varchar2(30), expert varchar2(30));
+insert into topic_experts values ('Puxxo', 'Np Bwwr');
+insert into topic_experts values ('Tdgune','Vofjfw Nov');
