ceda51fc2485e158d41f371078a24a316a307993
Michael Haynes <mf.haynes@gmail.com>
2021-02-08 13:39:55 -0500

    Update index_demo_script.sql

--- index_demo_script.sql
+++ index_demo_script.sql
@@ -1,32 +1,30 @@
-set pause on
 set autotrace on
-REM Query based on location
+pause Query based on location
 select books.book_data.title from books_j books where books.book_data.location = 'CB4';
 
-REM Create Location INDEX
+pause Create Location INDEX
 create index books_location_idx 
 on books_j (json_value(book_data, '$.location' ERROR ON ERROR NULL ON EMPTY));
 
-REM Re-Query Based on location
-REM Note still hitting table
+pause Re-Query Based on location
 select books.book_data.title from books_j books where books.book_data.location = 'CB4';
 
-REM Build new index with location and title
-REM Note needing to specify VARCHAR2 length to avoid key being too long.
+pause Note still hitting table
+pause Build new index with location and title
 create index books_location_title_idx 
 on books_j (json_value(book_data, '$.location' RETURNING VARCHAR2(3) ERROR ON ERROR NULL ON EMPTY),
             json_value(book_data, '$.title' ERROR ON ERROR NULL ON EMPTY));
+pause Note needing to specify VARCHAR2 length to avoid key being too long.
 
-REM New Index Gets Used	
-REM Note STILL hitting table...
 select books.book_data.title from books_j books where books.book_data.location = 'CB4';
+pause New Index Gets Use but STILL hitting table...
 
-REM Use syntax from Index DDL in Query
-REM No longer hits table.	
+pause Use syntax from Index DDL in Query	
 select json_value(book_data, '$.title' ERROR ON ERROR NULL ON EMPTY) from books_j books where books.book_data.location = 'CB4'			
+pause No longer hits table.
 
-REM Search Indexes allow full-text word searching (also supports Dataguide)
+pause Search Indexes allow full-text word searching (also supports Dataguide)
 CREATE SEARCH INDEX books_search_idx ON books_j (book_data) FOR JSON PARAMETERS ('DATAGUIDE ON');   
 
-REM Query with Search Index
+pause Query with Search Index
 select json_query(book_data,'$') as book_info from books_j where json_textcontains(book_data, '$', 'Jan');
