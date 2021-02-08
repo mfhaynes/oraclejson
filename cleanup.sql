96500db2e8ec7af79b06e98c1a00c553bd0cdde6
Michael Haynes <mf.haynes@gmail.com>
2021-02-08 14:31:49 -0500

    Update cleanup.sql

--- cleanup.sql
+++ cleanup.sql
@@ -1,9 +1,10 @@
+delete from books_j where "BOOK_DATA$publisher" is NOT NULL;
 drop table books_old;
-alter table books_j rename to books_old;
-alter table books_old drop constraint books_j_ck;
-create table books_j as select book_id, book_data from books_old;
-alter table books_j add constraint books_j_ck CHECK (book_data IS JSON);
 drop index books_search_idx;
 drop index books_location_idx;
 drop index books_location_title_idx;
 drop index books_location_vc_idx;
+alter table books_j rename to books_old;
+alter table books_old drop constraint books_j_ck;
+create table books_j as select book_id, book_data from books_old;
+alter table books_j add constraint books_j_ck CHECK (book_data IS JSON);
