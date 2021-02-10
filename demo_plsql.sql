4e20035a4cedfe402d0101eb96452ef8d6beb87e
Michael Haynes <mf.haynes@gmail.com>
2021-02-10 08:56:38 -0500

    Update demo_plsql.sql

--- demo_plsql.sql
+++ demo_plsql.sql
@@ -9,6 +9,7 @@ all_cost as (select sum(books.book_data.cost) total_cost from books_j books)
 select all_cost.total_cost-(.1*cost_over_70.total_cost) estimated_final
 from cost_over_70 cross join all_cost;
 
+pause Take 10% off the price of all books over $70
 DECLARE
     book_json            json_object_t;
     book_json_string     varchar2(4000);
