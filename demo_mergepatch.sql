355a2d75885efa4a33f9ea710161b59dae5e34ad
Michael Haynes <mf.haynes@gmail.com>
2021-02-10 08:55:01 -0500

    Update demo_mergepatch.sql

--- demo_mergepatch.sql
+++ demo_mergepatch.sql
@@ -1,6 +1,6 @@
 col inventory_amt for a15
-set lines 210
-set pages 50
+set lines 155
+set pages 38
 col title for a30
 select json_value(book_data,'$.title') as title, json_value(book_data,'$.inventoryamount') as inventory_amt
 from books_j where json_value(book_data,'$.inventoryamount' returning number) > 100;
@@ -9,4 +9,4 @@ set book_data = json_mergepatch(book_data,'{"inventoryamount":100}')
 where json_value(book_data,'$.inventoryamount' returning number) > 100;
 select json_value(book_data,'$.title') as title, json_value(book_data,'$.inventoryamount') as inventory_amt
 from books_j where json_value(book_data,'$.inventoryamount' returning number) > 100;
-select books.book_data.inventoryamount from books_j books where books.book_data.title = 'Vwlab sb Deuqg';
+select books.book_data.inventoryamount as inventory_amt from books_j books where books.book_data.title = 'Vwlab sb Deuqg';
